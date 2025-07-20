2a6bbb9ca6fd8e4fe3838826828fecf881f27191
Javi <javierezpeleta@gmail.com>
2025-07-20 18:31:59 +0200

    fix: post with multiple videos will only autoplay the first video

--- src/components/post/PostMetadataView.tsx
+++ src/components/post/PostMetadataView.tsx
@@ -346,7 +346,7 @@ const MediaGallery = ({ items }: { items: MediaAttachment[] }) => {
               />
             ) : (
               <div className="h-full flex items-center" style={{ height: "300px" }}>
-                <VideoPlayer url={item.item} preview="" galleryItems={items} currentIndex={index} autoplay={true} />
+                <VideoPlayer url={item.item} preview="" galleryItems={items} currentIndex={index} autoplay={index === 0} />
               </div>
             )}
           </div>
